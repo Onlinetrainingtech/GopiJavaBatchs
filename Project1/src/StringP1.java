63390abebcc1cc0c65a622a094d953e08e7bb1e1
azartechnet <azartechnet@gmail.com>
2024-07-09 07:32:28 +0530

    updated

---
+++ Project1/src/StringP1.java
@@ -0,0 +1,34 @@
+
+public class StringP1 {
+
+	public static void main(String[] args) {
+		
+		String s1="admin";
+		String s2=new String("admin");
+		String s3="Welcome to the Application";
+//		System.out.println(s1);
+//		System.out.println(s2);
+//		System.out.println("Length::"+s1.length());
+//		System.out.println("SpecificIndex::"+s1.charAt(0));
+//		System.out.println(s1.equals(s2));
+//		System.out.println(s3.startsWith("W"));
+//		System.out.println(s3.endsWith("n"));
+//		System.out.println(s3.indexOf('W'));
+		
+		
+		int a=10;
+
+		double b=2.0;
+
+		char[] arr={'a','b'};
+
+		System.out.println("Return value:"+String.valueOf(a));
+
+
+		System.out.println("Return value:"+String.valueOf(b));
+
+
+		System.out.println("Return value:"+String.valueOf(arr));
+	}
+
+}
---
+++ Project1/src/StringP2.java
@@ -0,0 +1,16 @@
+
+public class StringP2 {
+
+	public static void main(String[] args) {
+		
+	     StringBuilder sb=new StringBuilder("Fruits");
+		 System.out.println(sb.append("welcome"));
+		 System.out.println(sb.delete(6, 10));
+		 System.out.println(sb.insert(6,"azar"));
+		 System.out.println(sb.reverse());
+		 
+
+
+	}
+
+}
